54276cd339f5f59052a128a510a4bb31c3ee1fa5
Roy-Longarzo <roylongarzojr@gmail.com>
2025-04-16 00:01:43 -0400

    Moving Findaverage from private repo to public

---
+++ src/findaverage.py
@@ -0,0 +1,5 @@
+# find average function
+def findaverage(a, b, c):
+  # calculate avg of 3 numbers by adding them up and dividing them by 3
+  # return float so that decimal places are included
+    return float((a + b + c) / 3)
